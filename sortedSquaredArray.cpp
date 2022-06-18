f7dce064c7b533786bbf844605c2d3a3d233062c
Sadman Mehrab <sadmannaqueeb@gmail.com>
2022-06-18 19:19:19 +0600

    Update sortedSquaredArray.cpp

--- sortedSquaredArray.cpp
+++ sortedSquaredArray.cpp
@@ -7,24 +7,27 @@ Sorted Squared Array (18/06/22, Sadman Mehrab)
 #include<iostream>
 #include<vector>
 #include<math.h>
+
 #define HibanaUwU ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
 
 vector <int> sortedSquaredArray(vector<int> arr){
-    vector<int> newArr;
+    vector<int> newArr(arr.size());
     int l = 0;
     int r = arr.size()-1;
-    while(l<r){
+    int i = arr.size()-1;
+    while(i>=0){
         if(abs(arr[l]) < abs(arr[r]) ) {
-            newArr.insert(newArr.begin(),arr[r]*arr[r]);
+            newArr[i] = arr[r]*arr[r];
             r--;
         }
-        else if(abs(arr[l]) > abs(arr[r]) ) {
-            newArr.insert(newArr.begin(),arr[l]*arr[l]);
+        else if( abs(arr[l]) > abs(arr[r]) ) {
+            newArr[i] = arr[l]*arr[l];
             l++;
         }
+        else newArr[i] = arr[l]*arr[l];
+        i--;
     }
-    newArr.insert(newArr.begin(),arr[l]*arr[l]);
     return newArr;
 }
 
@@ -32,7 +35,7 @@ vector <int> sortedSquaredArray(vector<int> arr){
 int main(){
 
     HibanaUwU
-    vector<int> arr = {-11,-7,-2,3,5,6,8,9,10};
+    vector<int> arr = {-11,-7,-2,1,5,6,8,9,10};
     for(int x: arr) cout<<x<<" "; cout<<"\n";
 
     vector<int> arrSquared = sortedSquaredArray(arr);
